040fa1ba857a47dd41fb181abc91b9ceffa8ff7c
Yunkyungii <uk000509@gmail.com>
2023-06-30 14:24:07 +0900

    20230630 반응형까지 했음

--- js/custom.js
+++ js/custom.js
@@ -16,10 +16,37 @@ rightArrow.addEventListener('click', function () {
 
 const proSlide = new Swiper('.pro_slide', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 30,
     pagination: {
         el: ".sbar",
         type: "progressbar",
     },
+    breakpoints: {
+        540: {
+            slidesPerView: 4,
+        }
+    }
+
+    // scrollbar: {
+    //     el: ".drag"
+    //     draggable: true,
+    //     dragSize: 150,
+    //     hide: false,
+    // }
+})
+
+const mobileBtn = document.querySelector('.mobile_btn');
+const GNB = document.querySelector('#gnb');
+const H1 = document.querySelector('h1')
+
+mobileBtn.addEventListener('click', function () {
+    GNB.classList.toggle('on');
+    H1.classList.toggle('on');
+});
+
+GNB.addEventListener('wheel', function (e) {
+    if (GNB.classList.contains('on')) {
+        e.preventDefault();
+    }
 })
